4a45879354b8563f32b653123b3ab86717d34f39
adrianlemess <adrianlemess@gmail.com>
2022-02-27 21:10:01 +0100

    feat: add header and footer layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
     declarations: [HeaderComponent, FooterComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, MatToolbarModule, MatSidenavModule],
+    exports: [HeaderComponent, FooterComponent],
 })
 export class LayoutModule {}
