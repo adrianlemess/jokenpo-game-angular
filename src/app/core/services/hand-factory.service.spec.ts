01b532afd59aedb5a3957e7f58e7296fb9e70fe0
adrianlemess <adrianlemess@gmail.com>
2022-02-27 20:20:00 +0100

    test: update variables name

--- src/app/core/services/hand-factory.service.spec.ts
+++ src/app/core/services/hand-factory.service.spec.ts
@@ -17,65 +17,65 @@ describe('HandFactoryService', () => {
     });
 
     it('Should be tie when paper is against paper', () => {
-        const fistPlayer = service.create(HAND_TYPE.PAPER);
-        const secondPlayer = service.create(HAND_TYPE.PAPER);
+        const fistPlayerHand = service.create(HAND_TYPE.PAPER);
+        const secondPlayerHand = service.create(HAND_TYPE.PAPER);
 
-        expect(fistPlayer.playAgainst(secondPlayer)).toEqual(END_GAME_RESULT.TIE);
+        expect(fistPlayerHand.playAgainst(secondPlayerHand)).toEqual(END_GAME_RESULT.TIE);
     });
 
     it('Should be tie when rock is against rock', () => {
-        const fistPlayer = service.create(HAND_TYPE.ROCK);
-        const secondPlayer = service.create(HAND_TYPE.ROCK);
+        const fistPlayerHand = service.create(HAND_TYPE.ROCK);
+        const secondPlayerHand = service.create(HAND_TYPE.ROCK);
 
-        expect(fistPlayer.playAgainst(secondPlayer)).toEqual(END_GAME_RESULT.TIE);
+        expect(fistPlayerHand.playAgainst(secondPlayerHand)).toEqual(END_GAME_RESULT.TIE);
     });
 
     it('Should be tie when scissor is against scissor', () => {
-        const fistPlayer = service.create(HAND_TYPE.SCISSOR);
-        const secondPlayer = service.create(HAND_TYPE.SCISSOR);
+        const fistPlayerHand = service.create(HAND_TYPE.SCISSOR);
+        const secondPlayerHand = service.create(HAND_TYPE.SCISSOR);
 
-        expect(fistPlayer.playAgainst(secondPlayer)).toEqual(END_GAME_RESULT.TIE);
+        expect(fistPlayerHand.playAgainst(secondPlayerHand)).toEqual(END_GAME_RESULT.TIE);
     });
 
     it('Should be win for scissor when is against paper', () => {
-        const fistPlayer = service.create(HAND_TYPE.SCISSOR);
-        const secondPlayer = service.create(HAND_TYPE.PAPER);
+        const fistPlayerHand = service.create(HAND_TYPE.SCISSOR);
+        const secondPlayerHand = service.create(HAND_TYPE.PAPER);
 
-        expect(fistPlayer.playAgainst(secondPlayer)).toEqual(END_GAME_RESULT.WIN);
+        expect(fistPlayerHand.playAgainst(secondPlayerHand)).toEqual(END_GAME_RESULT.WIN);
     });
 
     it('Should be win for rock when is against scissor', () => {
-        const fistPlayer = service.create(HAND_TYPE.ROCK);
-        const secondPlayer = service.create(HAND_TYPE.SCISSOR);
+        const fistPlayerHand = service.create(HAND_TYPE.ROCK);
+        const secondPlayerHand = service.create(HAND_TYPE.SCISSOR);
 
-        expect(fistPlayer.playAgainst(secondPlayer)).toEqual(END_GAME_RESULT.WIN);
+        expect(fistPlayerHand.playAgainst(secondPlayerHand)).toEqual(END_GAME_RESULT.WIN);
     });
 
     it('Should be win for paper when is against rock', () => {
-        const fistPlayer = service.create(HAND_TYPE.PAPER);
-        const secondPlayer = service.create(HAND_TYPE.ROCK);
+        const fistPlayerHand = service.create(HAND_TYPE.PAPER);
+        const secondPlayerHand = service.create(HAND_TYPE.ROCK);
 
-        expect(fistPlayer.playAgainst(secondPlayer)).toEqual(END_GAME_RESULT.WIN);
+        expect(fistPlayerHand.playAgainst(secondPlayerHand)).toEqual(END_GAME_RESULT.WIN);
     });
 
     it('Should be defeat for paper when is against scissor', () => {
-        const fistPlayer = service.create(HAND_TYPE.PAPER);
-        const secondPlayer = service.create(HAND_TYPE.SCISSOR);
+        const fistPlayerHand = service.create(HAND_TYPE.PAPER);
+        const secondPlayerHand = service.create(HAND_TYPE.SCISSOR);
 
-        expect(fistPlayer.playAgainst(secondPlayer)).toEqual(END_GAME_RESULT.DEFEAT);
+        expect(fistPlayerHand.playAgainst(secondPlayerHand)).toEqual(END_GAME_RESULT.DEFEAT);
     });
 
     it('Should be defeat for scissor when is against rock', () => {
-        const fistPlayer = service.create(HAND_TYPE.SCISSOR);
-        const secondPlayer = service.create(HAND_TYPE.ROCK);
+        const fistPlayerHand = service.create(HAND_TYPE.SCISSOR);
+        const secondPlayerHand = service.create(HAND_TYPE.ROCK);
 
-        expect(fistPlayer.playAgainst(secondPlayer)).toEqual(END_GAME_RESULT.DEFEAT);
+        expect(fistPlayerHand.playAgainst(secondPlayerHand)).toEqual(END_GAME_RESULT.DEFEAT);
     });
 
     it('Should be defeat for rock when is against paper', () => {
-        const fistPlayer = service.create(HAND_TYPE.ROCK);
-        const secondPlayer = service.create(HAND_TYPE.PAPER);
+        const fistPlayerHand = service.create(HAND_TYPE.ROCK);
+        const secondPlayerHand = service.create(HAND_TYPE.PAPER);
 
-        expect(fistPlayer.playAgainst(secondPlayer)).toEqual(END_GAME_RESULT.DEFEAT);
+        expect(fistPlayerHand.playAgainst(secondPlayerHand)).toEqual(END_GAME_RESULT.DEFEAT);
     });
 });
