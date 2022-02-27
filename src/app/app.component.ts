4e743380027e95e930a1bbf06b81c64f740c8500
adrianlemess <adrianlemess@gmail.com>
2022-02-27 18:10:09 +0100

    refactor: rename folder shared to core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StrategyError } from './core/errors';
 
 @Component({
     selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'jokenpo';
+
+    constructor() {}
 }
