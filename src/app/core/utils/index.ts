ee99b1fa36e4ef783ef723c67b8d4026b705905b
adrianlemess <adrianlemess@gmail.com>
2022-02-28 20:15:02 +0100

    feat: add randomEnum utils

---
+++ src/app/core/utils/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Function to return a enum.
+ * Get from: https://stackoverflow.com/questions/44230998/how-to-get-a-random-enum-in-typescript
+ * I've decided to isolate this logic in a function to facilitate further tests
+ */
+
+export const randomEnum = <T>(anEnum: T): T[keyof T] => {
+    const enumValues = Object.values(anEnum) as unknown as T[keyof T][];
+    const randomIndex = Math.floor(Math.random() * enumValues.length);
+
+    return enumValues[randomIndex];
+};
