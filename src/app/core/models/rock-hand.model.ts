06e1221efe0ccb3f8527bd04f1f84c0332fdefcc
adrianlemess <adrianlemess@gmail.com>
2022-02-28 23:54:15 +0100

    feat: add image URL on models

--- src/app/core/models/rock-hand.model.ts
+++ src/app/core/models/rock-hand.model.ts
@@ -2,7 +2,7 @@ import { HAND_TYPE } from '../enums';
 import { JokenpoStrategy } from '../interfaces';
 import { AbstractHand } from './hand.model';
 
-const IMAGE_URL = '';
+const IMAGE_URL = '../../../assets/hand-rock.png';
 
 export class RockHand extends AbstractHand {
     constructor(strategy: JokenpoStrategy) {
