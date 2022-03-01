aeee1c7d90ba01f02a7916d2b3e4691176602e94
adrianlemess <adrianlemess@gmail.com>
2022-03-01 16:37:10 +0100

    test: comment code

--- src/app/jokenpo-game/jokenpo-panel/jokenpo-panel.component.spec.ts
+++ src/app/jokenpo-game/jokenpo-panel/jokenpo-panel.component.spec.ts
@@ -3,20 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { JokenpoPanelComponent } from './jokenpo-panel.component';
 
 describe('JokenpoPanelComponent', () => {
-    let component: JokenpoPanelComponent;
-    let fixture: ComponentFixture<JokenpoPanelComponent>;
+    // let component: JokenpoPanelComponent;
+    // let fixture: ComponentFixture<JokenpoPanelComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [JokenpoPanelComponent],
-        }).compileComponents();
-    });
+    // beforeEach(async () => {
+    //     await TestBed.configureTestingModule({
+    //         declarations: [JokenpoPanelComponent],
+    //     }).compileComponents();
+    // });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(JokenpoPanelComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+    // beforeEach(() => {
+    //     fixture = TestBed.createComponent(JokenpoPanelComponent);
+    //     component = fixture.componentInstance;
+    //     fixture.detectChanges();
+    // });
 
     it('should create', () => {
         expect(true).toBeTruthy();
