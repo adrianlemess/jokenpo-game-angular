0498509f7cbcae12449fa5d03912db6a93b72c99
adrianlemess <adrianlemess@gmail.com>
2022-03-02 17:52:17 +0100

    test: add tests to carousel component

--- src/app/shared/hands-carousel/hands-carousel.component.spec.ts
+++ src/app/shared/hands-carousel/hands-carousel.component.spec.ts
@@ -1,23 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HandFactoryService } from '../../core/services/hand-factory.service';
 
 import { HandsCarouselComponent } from './hands-carousel.component';
 
 describe('HandsCarouselComponent', () => {
-    // let component: HandsCarouselComponent;
-    // let fixture: ComponentFixture<HandsCarouselComponent>;
-    // beforeEach(async () => {
-    //     await TestBed.configureTestingModule({
-    //         declarations: [HandsCarouselComponent],
-    //         imports: [NgbModule],
-    //     }).compileComponents();
-    // });
-    // beforeEach(() => {
-    //     fixture = TestBed.createComponent(HandsCarouselComponent);
-    //     component = fixture.componentInstance;
-    //     fixture.detectChanges();
-    // });
-    it('should create', () => {
-        expect(true).toBeTruthy();
+    let component: HandsCarouselComponent;
+    let fixture: ComponentFixture<HandsCarouselComponent>;
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [HandsCarouselComponent],
+            imports: [NgbModule],
+        }).compileComponents();
+    });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HandsCarouselComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('Should render with no hands and only carouselTitle and subtitle', () => {
+        component.carouselTitle = 'Carousel title';
+        component.carouselSubtitle = 'Carousel subtitle';
+        component.hands = [];
+
+        expect(fixture.nativeElement).toMatchSnapshot('Hands empty');
+    });
+
+    it('Should render with a list of hands', () => {
+        const handsFactoryService = TestBed.inject(HandFactoryService);
+
+        component.hands = handsFactoryService.getAllHands();
+        component.carouselTitle = 'Carousel title';
+        component.carouselSubtitle = 'Carousel subtitle';
+
+        expect(fixture.nativeElement).toMatchSnapshot('Hands list');
     });
 });
