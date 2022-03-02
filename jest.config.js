358f7e764ebbaadcd1d631d30f04d44af57a12fb
adrianlemess <adrianlemess@gmail.com>
2022-03-02 17:44:18 +0100

    config: update test configs

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
     globalSetup: 'jest-preset-angular/global-setup',
-    modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/cypress'],
+    modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/cypress', '<rootDir>/testing'],
 };
--- setup-jest.ts
+++ setup-jest.ts
@@ -1,1 +1,4 @@
+import '@angular/localize/init';
+import '@testing-library/jest-dom/extend-expect';
+
 import 'jest-preset-angular/setup-jest';
